f5113d20432b398de2c9cc45a70cdbb688dc4807
ITR <ITR@live.no>
2017-08-28 13:41:31 +0200

    Worked more on ghost-leg

--- ghostleg.go
+++ ghostleg.go
@@ -31,30 +31,31 @@ type GhostLeg struct {
 
 func GetRandomGhostLeg(width, height int) *GhostLeg {
 	flips := make([][]bool, height)
-	hits := [3]int{1, 1, 1}
+	hits := make([]int, width)
+
+	c := 2
 	for i := range flips {
 		flips[i] = make([]bool, width-1)
-		total := hits[0] + hits[1]*2 + hits[2]*3
-		chances := [3]int{total - hits[0],
-			total - hits[1]*2, total - hits[2]*3}
-		left := 0
 		for j := 0; j < len(flips[i]); j++ {
-			if left == 0 {
-				r := rand.Int() % total
-				for k := range chances {
-					if chances[k] > r {
-						left = k
-						hits[k]++
-						break
-					}
-					r -= chances[k]
-				}
+			top, bottom := hits[j]+hits[j+1], c
+			if top < 0 {
+				bottom -= top
+				top = 1
 			} else {
-				left--
+				bottom += top
 			}
-			if left == 0 {
+
+			if rand.Int()%bottom < top {
+				c++
+				hits[j], hits[j+1] = hits[j+1]-3, hits[j]-3
+			} else {
+				c = 2
 				flips[i][j] = true
+				hits[j], hits[j+1] = hits[j+1]+3, hits[j]+3
 				j++
+				if len(hits) < j+1 {
+					hits[j]--
+				}
 			}
 		}
 	}
@@ -62,17 +63,15 @@ func GetRandomGhostLeg(width, height int) *GhostLeg {
 	return &GhostLeg{width, flips, rand.Int() % width}
 }
 
-func (gl *GhostLeg) Display(renderer *sdl.Renderer) {
+func (gl *GhostLeg) DisplayPath(path int, renderer *sdl.Renderer) {
 	renderer.SetDrawColor(0, 0, 0, 255)
 	renderer.SetRenderTarget(nil)
 	renderer.Clear()
 	renderer.SetDrawColor(255, 255, 255, 255)
 
-	s := gl.end
-
 	for i := range gl.flips {
 		for j := 0; j < gl.legs; j++ {
-			if j == s {
+			if j == path {
 				renderer.SetDrawColor(255, 0, 0, 255)
 			} else {
 				renderer.SetDrawColor(255, 255, 255, 255)
@@ -85,12 +84,12 @@ func (gl *GhostLeg) Display(renderer *sdl.Renderer) {
 
 		for j := range gl.flips[i] {
 			if gl.flips[i][j] {
-				if j == s {
+				if j == path {
 					renderer.SetDrawColor(255, 0, 0, 255)
-					s++
-				} else if j == s-1 {
+					path++
+				} else if j == path-1 {
 					renderer.SetDrawColor(255, 0, 0, 255)
-					s--
+					path--
 				} else {
 					renderer.SetDrawColor(255, 255, 255, 255)
 				}
@@ -102,4 +101,71 @@ func (gl *GhostLeg) Display(renderer *sdl.Renderer) {
 		}
 	}
 	renderer.Present()
+}
+
+func (gl *GhostLeg) AnimatePath(path int) func(*sdl.Renderer) (int, int) {
+	depth := 0
+	skipped := 0
+	return func(renderer *sdl.Renderer) (int, int) {
+		s := path
+		renderer.SetDrawColor(0, 0, 0, 255)
+		renderer.Clear()
+		renderer.SetDrawColor(255, 255, 255, 255)
+
+		for i := range gl.flips {
+			for j := 0; j < gl.legs; j++ {
+				if j == s && i <= depth/2 {
+					renderer.SetDrawColor(255, 0, 0, 255)
+				} else {
+					renderer.SetDrawColor(255, 255, 255, 255)
+				}
+				renderer.FillRect(&sdl.Rect{
+					int32(j) * screenWidth / int32(gl.legs),
+					int32(i) * screenHeight / int32(len(gl.flips)),
+					4, screenHeight / int32(gl.legs)})
+			}
+
+			for j := range gl.flips[i] {
+				if gl.flips[i][j] {
+					if j == s && i*2 < depth {
+						renderer.SetDrawColor(255, 0, 0, 255)
+						s++
+					} else if j == s-1 && i*2 < depth {
+						renderer.SetDrawColor(255, 0, 0, 255)
+						s--
+					} else {
+						renderer.SetDrawColor(255, 255, 255, 255)
+					}
+					renderer.FillRect(&sdl.Rect{
+						int32(j) * screenWidth / int32(gl.legs),
+						int32(i+1) * screenHeight / int32(len(gl.flips)),
+						screenWidth / int32(gl.legs), 4})
+				}
+			}
+		}
+
+		if depth%2 != 0 {
+			depth++
+		} else if depth/2 < len(gl.flips) &&
+			((s > 0 && gl.flips[depth/2][s-1]) ||
+				(s < len(gl.flips[depth/2]) && gl.flips[depth/2][s])) {
+			depth++
+		} else {
+			skipped++
+			depth += 2
+		}
+
+		if depth < len(gl.flips)*2 {
+			return -1, 1
+		}
+		return s, skipped
+	}
+}
+
+func (gl *GhostLeg) Choose(renderer *sdl.Renderer) int {
+
+}
+
+func (gl *GhostLeg) Play(renderer *sdl.Renderer) {
+
 }
